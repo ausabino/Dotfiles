8235386dab346957af37f26d151c87dcd8f7399f
Alan Sabino <alan.utsuni.sabino@gmail.com>
2022-01-31 18:35:49 -0300

    Add modules to doom emacs

--- doom-emacs/init.el
+++ doom-emacs/init.el
@@ -87,9 +87,9 @@
        ;;ansible
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       ;;docker
+       docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
-       ;;ein               ; tame Jupyter notebooks with emacs
+       ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
